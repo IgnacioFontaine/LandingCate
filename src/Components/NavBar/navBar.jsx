534a27493752bd6da59851769717ea424bff616f
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-27 11:15:15 -0300

    creando archivos

---
+++ src/Components/NavBar/navBar.jsx
@@ -0,0 +1,19 @@
+import "./navBar.css"
+
+const NavBar = () => {
+
+  return (
+    <section className="navbar-container">
+      <ul>
+        <li>Inicio</li>
+        <li>Iniciate</li>
+        <li>Cate</li>
+        <li>Apostolate</li>
+        <li>Ropa</li>
+        <li>Documentos</li>
+      </ul>
+    </section>
+  )
+}
+
+export default NavBar;
