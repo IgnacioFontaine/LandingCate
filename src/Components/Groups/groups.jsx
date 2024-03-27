dfbd78777e43df545b69a44dcadc4ca03ee59e4f
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-27 15:02:09 -0300

    cambio en iniciate vew

--- src/Components/Groups/groups.jsx
+++ src/Components/Groups/groups.jsx
@@ -1,10 +1,25 @@
 import "./groups.css"
 
-function Groups() {
+function CardGroup({id, nombre}) {
   
   return (
-    <div className="groups-container">
-      
+    <div className="Group" key={id}>
+      <h2 className="name-group">{nombre}</h2>
+    </div>
+  )
+}
+
+function Groups({grupos}) {
+  
+  return (
+    <div >
+      <section id="locations-contenedor" className="cont-groups" >
+        {grupos.map(group =>
+          <CardGroup
+            key={group.id}
+            nombre={group.nombre}
+            className="location-card" />)}
+      </section>
     </div>
   )
 }
--- src/Views/Iniciate/iniciate.jsx
+++ src/Views/Iniciate/iniciate.jsx
@@ -1,12 +1,27 @@
 import Hero from "../../Components/Hero/hero";
 import "./iniciate.css"
 import iniciateImage from "../../assets/LogoBlancoCruz.png"
+import Groups from "../../Components/Groups/groups";
 
 const Iniciate = () => {
+  const iniciate_groups =
+    [{ id: 1, nombre: "REPADI" },
+      { id: 2, nombre: "MACADI" },
+      { id: 3, nombre: "HELAFE" },
+      { id: 4, nombre: "FRACODI" },
+      { id: 5, nombre: "REPADI" },
+      { id: 6, nombre: "REPADI" },
+      { id: 7, nombre: "REPADI" },
+      { id: 8, nombre: "REPADI" },
+      { id: 9, nombre: "REPADI" },
+      { id: 10, nombre: "REPADI" },
+      { id: 11, nombre: "REPADI" },
+      { id: 12, nombre: "REPADI" },]
 
   return (
     <section className="iniciate-container">
       <Hero title={"Iniciate"} imagen={iniciateImage} />
+      <Groups grupos={iniciate_groups} />
     </section>
   )
 }
