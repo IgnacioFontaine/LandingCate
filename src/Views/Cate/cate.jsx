abb658a25112e62f0d2fa6996110685066572e25
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-28 11:57:21 -0300

    cate view en proceso

--- src/Views/Cate/cate.jsx
+++ src/Views/Cate/cate.jsx
@@ -6,18 +6,18 @@ import cateImage from "../../assets/LogoBlancoCruz.png"
 const Cate = () => {
 
   const cate_groups =
-    [{ id: 1, nombre: "CAFEDI" },
-      { id: 2, nombre: "TALOCU" },
-      { id: 3, nombre: "SENOGUI" },
-      { id: 4, nombre: "CAMIFE" },
-      { id: 5, nombre: "SOSACRU" },
-      { id: 6, nombre: "TUFEGUI" },
-      { id: 7, nombre: "REPADI" },
-      { id: 8, nombre: "REPADI" },
-      { id: 9, nombre: "REPADI" },
-      { id: 10, nombre: "REPADI" },
-      { id: 11, nombre: "REPADI" },
-      { id: 12, nombre: "CAMIFE" },]
+    [ { id: 1, nombre: "CAFEDI", significado: "Construyendo Amistad en la Fe de Dios" },
+      { id: 2, nombre: "NUCAVI", significado: "Nuestro Camino, Verdad y Vida" },
+      { id: 3, nombre: "TALOCU", significado: "Testigos de tu Amor y Locura" },
+      { id: 4, nombre: "TESANTU", significado: "Testigos del Santo Espíritu" },
+      { id: 5, nombre: "SENOGUI", significado: "El Señor nos Guía" },
+      { id: 6, nombre: "CONJUFE", significado: "Contagiamos Juntos la Fe" },
+      { id: 7, nombre: "FEJUDI", significado: "Felices Junto a Dios" },
+      { id: 8, nombre: "UCOFE", significado: "Unidos Compartiendo la Fe" },
+      { id: 9, nombre: "CAMIFE", significado: "Caminantes en la Vida y la Fe" },
+      { id: 10, nombre: "AMIDIOS", significado: "Admirando la Humildad de Dios" },
+      { id: 11, nombre: "SOSACRU", significado: "Soldados de la Santa Cruz" },
+      { id: 12, nombre: "TUFEGUI", significado:"Tu Fe nos Guiará" },]
 
   return (
     <section className="cate-container">
