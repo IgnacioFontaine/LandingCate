f0d9d3ecd80b35f80b5554320be73904e09f6dff
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-09 12:07:01 -0300

    oración agregada

--- src/Components/Oracion/oracion.jsx
+++ src/Components/Oracion/oracion.jsx
@@ -12,19 +12,19 @@ const Oracion = () => {
           Gracias Padre por llamarnos a vivir en la Iglesia, familia de los que creen en Ti.<br/>
           Gracias por amarnos, darnos la vida, nuestras familias y el pan de cada día.<br/>
 
-          Gracias por La Cate que nos acerca a Ti.
+          Gracias por La Cate que nos acerca a Ti.<br/>
           Gracias por invitarnos a ser libres, felices y Santos.<br/>
 
           Señor, que cada uno de nosotros con la luz del Espíritu Santo, sea testimonio<br/>
           de Tu presencia y amor, y así hagamos de lo cotidiano la semilla de la santidad.<br/>
 
-          Padre nuestro que podamos crecer juntos en la fe,
+          Padre nuestro que podamos crecer juntos en la fe,<br/>
           abandonándonos en tu voluntad, en la esperanza siendo luz del mundo<br/>
 
           y en la caridad viviendo para dar la vida.<br/>
 
           Enseñanos a caminar en tu presencia y llenar nuestra vida de Ti,<br/>
-          como María madre de tu Hijo, madre y modelo de todos tus hijos.<br/>
+          como María madre de tu Hijo, madre y modelo de todos tus hijos.<br/><br/>
           ¡Alabado sea Jesucristo, por siempre sea alabado!
     </p>
   </div>)
