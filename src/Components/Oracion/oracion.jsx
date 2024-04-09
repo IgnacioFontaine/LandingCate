d4e06c8abc366888a7b75b80c1fe67638a27ebe3
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-09 11:51:45 -0300

    oración agregada

---
+++ src/Components/Oracion/oracion.jsx
@@ -0,0 +1,31 @@
+const Oracion = () => {
+  return (
+    <div>
+      <h2>ORACION DE LA CATE</h2>
+      <p>
+Señor, que en la cruz de tu Hijo Jesús nos mostraste tu grandeza y tu
+
+misericordia, queremos alabarte.
+
+Gracias Padre por llamarnos a vivir en la Iglesia, familia de los que creen en Ti.
+Gracias por amarnos, darnos la vida, nuestras familias y el pan de cada día.
+
+Gracias por La Cate que nos acerca a Ti.
+Gracias por invitarnos a ser libres, felices y Santos.
+
+Señor, que cada uno de nosotros con la luz del Espíritu Santo, sea testimonio
+de Tu presencia y amor, y así hagamos de lo cotidiano la semilla de la santidad.
+
+Padre nuestro que podamos crecer juntos en la fe,
+abandonándonos en tu voluntad, en la esperanza siendo luz del mundo
+
+y en la caridad viviendo para dar la vida.
+
+Enseñanos a caminar en tu presencia y llenar nuestra vida de Ti,
+como María madre de tu Hijo, madre y modelo de todos tus hijos.
+¡Alabado sea Jesucristo, por siempre sea alabado!
+    </p>
+  </div>)
+}
+
+export default Oracion;
