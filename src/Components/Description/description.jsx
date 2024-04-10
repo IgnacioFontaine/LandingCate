7f1f577ed042eb85fca8553abc61b40d22a2cfb8
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-10 19:57:21 -0300

    cambio description

--- src/Components/Description/description.jsx
+++ src/Components/Description/description.jsx
@@ -5,10 +5,14 @@ function Description() {
   return (
     <div className="description">
       <section  id="description-cate" className="description-cate"  >
-          <h2 >¿Qué es la Cate?</h2>
-          <p className="description-parragraph" >
+          <h2 className="description-title">¿Qué es la Cate?</h2>
+          {/* <p className="description-parragraph" >
           Somos un grupo Juvenil que se encuentra todos los Sábados por la tarde para reflexionar de un tema en particular, divididos por edades en grupos y compartir una merienda que entre risas y charlas transforma nuestra tarde en un momento lleno de Dios.
           Al año participamos de dos Campamentos que son una experiencia hermosa donde logramos compartir con otros grupos y vivir momentos únicos, llenos de Fe y amistad.
+        </p> */}
+        <p className="description-parragraph">
+          En La Cate, más que una comunidad, somos una gran familia de jóvenes católicos que se reúnen todos los sábados para crecer juntos en nuestro camino de Fe, compartiendo una merienda.
+          Entre risas, charlas, campamentos y apostolados, se esconde nuestro verdadero propósito de acercarnos a Aquel Amigo que dio la vida por nosotros primero
         </p>
       </section>
       </div>
