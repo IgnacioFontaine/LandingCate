303e846dca6b7b5a8015f723a0ad82f52e4d132a
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-04-10 18:58:21 -0300

    testimonios en proceso

--- src/Components/Testimonios/testimonios.jsx
+++ src/Components/Testimonios/testimonios.jsx
@@ -1,12 +1,20 @@
-const CardTestimonio = ({id, nombre, experiencia}) => {
+import "./testimonios.css"
+const CardTestimonio = ({ id, nombre, años, experiencia }) => {
   return (
-    <div className="testimonio-card">
-      <header className="testimonio-nombre">
-        <h2>Nombre</h2>
+    <div className="testimonio-card" key={id}>
+      <header className="testimonio-nombre" >
+        <div>
+          <h2 >{nombre}</h2>
+        </div>
+        <div className="testimonio-años">
+          <h3>
+            {años}
+          </h3>
+        </div>
       </header>
       <body>
         <p className="testimonio-experiencia">
-          Mi Experiencia en la Cate fue:
+          {experiencia}
         </p>
       </body>
     </div>
@@ -16,12 +24,14 @@ const CardTestimonio = ({id, nombre, experiencia}) => {
 function Testimonios({testimonios}) {
   
   return (
-    <div >
-      <section id="locations-contenedor" className="cont-groups" >
+    <div className="contenedor-testi" >
+      <h2 className="title-testimonios">Testimonios de personas que pasaron por la Cate</h2>
+      <section id="testi-contenedor" className="cont-testimonios" >
         {testimonios.map(testi =>
           <CardTestimonio
             key={testi.id}
             nombre={testi.nombre}
+            años={testi.años}
             experiencia={testi.experiencia}
              />)}
       </section>
--- src/Views/Iniciate/iniciate.jsx
+++ src/Views/Iniciate/iniciate.jsx
@@ -2,6 +2,7 @@ import Hero from "../../Components/Hero/hero";
 import "./iniciate.css"
 import iniciateImage from "../../assets/LogoBlancoCruz.png"
 import Groups from "../../Components/Groups/groups";
+import Testimonios from "../../Components/Testimonios/testimonios";
 
 const Iniciate = () => {
 
@@ -11,12 +12,15 @@ const Iniciate = () => {
       { id: 2, nombre: "MIFEDIOS ", significado: "Misioneros de la Felicidad de Dios" },
       { id: 3, nombre: "FRACODI ", significado: "Fraterno Corazón de Jesús" },
       { id: 4, nombre: "COMADI", significado: "Nuestro Corazon en las manos de Dios" },
-      { id: 5, nombre: "LUDECRI", significado:"Luces de Cristo" },]
+      { id: 5, nombre: "LUDECRI", significado: "Luces de Cristo" },]
+  
+  const testimonios_cate = [{id:60, nombre:"Ignacio Fontaine", años:"2015-2019" ,experiencia:"Entré a la Cate en 2015 y desde entonces es un pilar fundamental en mi Vida y mi Fe, los Silucri mi grupo son personas muy importantes para mi. "},{id:60, nombre:"Ignacio Fontaine", años:"2015-2019",experiencia:"Entré a la Cate en 2015 y desde entonces es un pilar fundamental en mi Vida y mi Fe, los Silucri mi grupo son personas muy importantes para mi. "}, {id:60, nombre:"Ignacio Fontaine", experiencia:"Entré a la Cate en 2015 y desde entonces es un pilar fundamental en mi Vida y mi Fe, los Silucri mi grupo son personas muy importantes para mi. "},{id:60, nombre:"Ignacio Fontaine", años:"2015-2019" ,experiencia:"Entré a la Cate en 2015 y desde entonces es un pilar fundamental en mi Vida y mi Fe, los Silucri mi grupo son personas muy importantes para mi. "}, {id:60, nombre:"Ignacio Fontaine", experiencia:"Entré a la Cate en 2015 y desde entonces es un pilar fundamental en mi Vida y mi Fe, los Silucri mi grupo son personas muy importantes para mi. "}]
 
   return (
     <section className="iniciate-container">
       <Hero title={"Grupos Iniciate"} imagen={iniciateImage} frase={frase} />
       <Groups grupos={iniciate_groups} />
+      <Testimonios testimonios={testimonios_cate} />
     </section>
   )
 }
